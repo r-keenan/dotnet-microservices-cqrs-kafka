1eb334aa764bc4598ee1b58cc33d32c9e38f468b
r-keenan <ross.keenan@protonmail.com>
2024-09-06 14:23:14 -0500

    Update first part of PostAggregate.

---
+++ SM-Post/Post.Cmd/Post.Cmd.Domain/Aggregates/PostAggregate.cs
@@ -0,0 +1,114 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CQRS.Core.Domain;
+using Post.Common.Events;
+
+namespace Post.Cmd.Domain
+{
+    public class PostAggregate: AggregateRoot
+    {
+        private bool _active; 
+        private string _author;
+        private readonly Dictionary<Guid, Tuple<string, string>> _comments = new(); 
+
+        public bool Active
+        {
+            get => _active; set => _active = value;
+        }
+
+        public PostAggregate() 
+        {
+
+        }
+
+        public PostAggregate(Guid id, string author, string message)
+        {
+            RaiseEvent(new PostCreatedEvent
+            {
+                Id = id,
+                Author = author,
+                Message = message,
+                DatePosted = DateTime.Now()
+            });
+        }
+
+        public void Apply(PostCreatedEvent @event)
+        {
+            _id = @event.Id;
+            _active = true;
+            _author = @event.Author;
+        }
+
+        public void EditMessage(string message)
+        {
+            if (!_active)
+            {
+                throw InvalidOperationException("You cannot edit the message of an inactive post!");
+            }
+
+            if (string.IsNullOrWhiteSpace(message))
+            {
+                throw InvalidOperationException($"The value of {nameof(message)} cannot be null or empty. Please provide a valid {nameof(message)}!");
+            }
+
+            RaiseEvent(new MessageUpdatedEvent
+            {
+                Id = id,
+                Message = message
+            });
+        }
+
+        public void Apply(MessageUpdatedEvent @event)
+        {
+            _id = @event.Id,
+        }
+    
+        public void LikePost()
+        {
+            if (!_active)
+            {
+                throw new InvalidOperationException("You cannot like an inactive post!");
+            }
+
+            RaiseEvent(new PostLikedEvent
+            {
+                Id = _id
+            });
+        }
+
+        public void Apply(PostLikedEvent @event)
+        {
+            _id = @event.Id;
+        }
+
+        public void AddComment(string comment, string username)
+        {
+            if (!_active)
+            {
+                throw new InvalidOperationException("You cannot add a comment to an inactive post!");
+            }
+
+            if (string.IsNullOrWhiteSpace(comment))
+            {
+                throw InvalidOperationException($"The value of {nameof(comment)} cannot be null or empty. Please provide a valid {nameof(comment)}!");
+            }
+
+            RaiseEvent(new CommentAddedEvent
+            {
+                Id = _id,
+                CommentId = Guid.NewGuid(),
+                Comment = comment,
+                Username = username,
+                CommentDate = DateTime.Now
+            });
+        }
+
+        public void Apply(CommentAddedEvent @event)
+        {
+            _id = @event.Id;
+            _comments.Add(@event.CommentId, new Tuple<string, string>(@event.Comment, @event.Username));
+        }
+    }
+}
