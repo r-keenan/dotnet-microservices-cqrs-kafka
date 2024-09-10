bb803e49320af4ea5a01cc8160bde36e95383a8b
r-keenan <ross.keenan@protonmail.com>
2024-09-10 13:01:09 -0500

    Wired the eventProducer into the EventStore.

--- SM-Post/Post.Cmd/Post.Cmd.Infrastructure/Stores/EventStore.cs
+++ SM-Post/Post.Cmd/Post.Cmd.Infrastructure/Stores/EventStore.cs
@@ -2,17 +2,20 @@ using CQRS.Core.Domain;
 using CQRS.Core.Events;
 using CQRS.Core.Exceptions;
 using CQRS.Core.Infrastructure;
+using CQRS.Core.Producers;
 using Post.Cmd.Domain;
 
 namespace Post.Cmd.Infrastructure.Stores;
 
 public class EventStore : IEventStore
 {
+    private readonly IEventProducer _eventProducer;
     private readonly IEventStoreRepository _eventStoreRepository;
 
-    public EventStore(IEventStoreRepository eventStoreRepository)
+    public EventStore(IEventStoreRepository eventStoreRepository, IEventProducer eventProducer)
     {
         _eventStoreRepository = eventStoreRepository;
+        _eventProducer = eventProducer;
     }
 
     public async Task SaveEventsAsync(Guid aggregateId, IEnumerable<BaseEvent> events, int expectedVersion)
@@ -39,6 +42,9 @@ public class EventStore : IEventStore
             };
 
             await _eventStoreRepository.SaveAsync(eventModel);
+
+            var topic = Environment.GetEnvironmentVariable("KAFKA_TOPIC");
+            await _eventProducer.ProduceAsync(topic, @event);
         }
     }
 
