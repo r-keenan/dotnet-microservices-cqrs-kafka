22bff431557fd384606936354c0ceee795500fda
r-keenan <ross.keenan@protonmail.com>
2024-09-10 08:59:51 -0500

    Updated from minimal API to API w/ controllers. Added EventStoreRepository and MongoDbConfig.

---
+++ CQRS-ES/CQRS.Core/Infrastructure/Config/MongoDbConfig.cs
@@ -0,0 +1,8 @@
+namespace CQRS.Core.Infrastructure.Config;
+
+public class MongoDbConfig
+{
+    public string ConnectionString { get; set; }
+    public string Database { get; set; }
+    public string Collection { get; set; }
+}
