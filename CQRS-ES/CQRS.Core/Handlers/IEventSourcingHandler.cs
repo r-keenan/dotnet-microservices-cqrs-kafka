d6c09e51bb68c6a4ff49963bf78583ca8390ed5a
r-keenan <ross.keenan@protonmail.com>
2024-09-10 11:14:21 -0500

    Added interface and implementation of EventSourcingHandler.

--- CQRS-ES/CQRS.Core/Handlers/IEventSourcingHandler.cs
+++ CQRS-ES/CQRS.Core/Handlers/IEventSourcingHandler.cs
@@ -5,5 +5,5 @@ namespace CQRS.Core.Handlers;
 public interface IEventSourcingHandler<T>
 {
     Task SaveAsync(AggregateRoot aggregate);
-    Task<T> GetByIdAsync(Guid id);
+    Task<T> GetByIdAsync(Guid aggregateId);
 }
